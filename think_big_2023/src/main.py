023c292e8a7bdf773df8fdb1672a4b3fd76aae98
josh <freckletonj@gmail.com>
2023-04-12 22:16:54 -0700

    cleanup

--- think_big_2023/src/main.py
+++ think_big_2023/src/main.py
@@ -45,23 +45,6 @@ async def on_ready():
     print('done')
     print('------')
 
-@bot.hybrid_command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-@bot.hybrid_command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
 
 ##################################################
 # Stable Diffusion
